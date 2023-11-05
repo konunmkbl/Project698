25780cdc2325ad967ff062fe5134af7531dc4ace
konunmkbl <145894831+konunmkbl@users.noreply.github.com>
2023-11-05 22:04:57 +0700

    Update app_creditcardfraud.py

--- app_creditcardfraud.py
+++ app_creditcardfraud.py
@@ -49,8 +49,8 @@ if st.button("Predict Fraud"):
         st.error(f"An error occurred: {e}")
 # Reset button to clear the input fields
 if st.button("Reset"):
-    mcc = st.text_input("MCC (Merchant Category Code (4Digit)):", value="")
-    zipcode = st.text_input("ZIP Code:", value="")
-    currency = st.text_input("Currency:", value="")
-    card_type = st.text_input("Card Type:", value="")
-    device = st.text_input("Device:", value="")
+    mcc = ""
+    zipcode = ""
+    currency = ""
+    card_type = ""
+    device = ""
